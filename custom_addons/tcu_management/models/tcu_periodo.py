581126fa858018abb059e0ba58e6610bb73d91c2
Luis Campos R <luiscmpsr@gmail.com>
2025-09-19 18:29:14 -0600

    Corregí la posición de definición de la función

--- custom_addons/tcu_management/models/tcu_periodo.py
+++ custom_addons/tcu_management/models/tcu_periodo.py
@@ -4,14 +4,14 @@ from datetime import datetime
 class TCUPeriodo(models.Model):
     _name = "tcu.periodo"
     _description = "TCU Periodo"
+
+    # Me da el año actual más los 5 siguientes
+    def _get_year_selection(self):
+        actual = datetime.today().year
+        return [(str(y), str(y)) for y in range(actual, actual + 5)]
     
     per_nombre      = fields.Char(string="Nombre",       required=True)
     per_activo      = fields.Boolean(string="Activo",    required=True)
     per_anno        = fields.Selection(string="Año",     required=True, selection=_get_year_selection)
     per_fechainicio = fields.Date(string="Fecha Inicio", required=True)
     per_fechaFinal  = fields.Date(string="Fecha Final",  required=True)
-        
-    # Me da el año actual más los 5 siguientes
-    def _get_year_selection(self):
-        actual = datetime.today().year
-        return [(str(y), str(y)) for y in range(actual, actual + 5)]
